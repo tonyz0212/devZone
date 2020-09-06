a4709b9288e6e04a57c96d3194dabab4f4ec5760
tony <tonyz0212@gmail.com>
2020-09-06 16:37:19 -0700

    first commit

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURL');
+
+const conenctDB = async () => {
+    try{
+      await mongoose.connect(db,{
+          useUnifiedTopology: true,
+          useNewUrlParser: true
+      });
+
+      console.log('MongoDB connected');
+    } catch(err){
+        console.log(err.message);
+        // Exit process with failure
+        process.exit(1);
+    }
+}
+
+
+module.exports = conenctDB;
