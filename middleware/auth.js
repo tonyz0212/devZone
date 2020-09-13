6f780a1967124881eb096118e865cf101fed734d
tony <tonyz0212@gmail.com>
2020-09-13 16:32:25 -0700

    implemented authentication, register and login

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,25 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 
-
-module.exports = function(req,res,next){
+module.exports = function (req, res, next) {
 
     // 从header获得token
     const token = req.header('x-auth-token');
 
     // check if no token
-    if(!token){
+    if (!token) {
         // not authorize
-        return res.status(401).json({msg: 'No token'});
+        return res.status(401).json({ msg: 'No token' });
     }
 
     // 有token的话，Verify
-    try{
-        const decoded = jwt.verify(token,config.get('jwtSecret'));
-
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
         req.user = decoded.user;
+        next();
 
-    } catch(err){
-       res.status(401).json({msg:'Token is not valid'}); 
+    } catch (err) {
+        res.status(401).json({ msg: 'Token is not valid' });
     }
 } 
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,85 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+const User = require('../../models/User');
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get('/',(req,res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+    try {
+        // 从 req.user 获取user的信息，但是不显示password
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
+
+// @route   POST api/auth
+// @desc    Autheticaet user and get token
+// @access  Public
+router.post('/',
+    // 检查输入的格式是否正确
+    [
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Password is required').exists()
+    ],
+    async (req, res) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name, email, password } = req.body;
+
+        try {
+            // 检查用户是否在
+            let user = await User.findOne({ email });
+
+            if (!user) {
+                return res.status(400).json({ errors: [{ msg: 'User does not exits OR Password is wrong' }] });
+            }
+
+
+            const matched = await bcrypt.compare(password, user.password);
+
+            // do not specify whether it is username or password issue.
+            if (!matched) {
+                return res.status(400).json({ errors: [{ msg: 'User does not exits OR Password is wrong' }] });
+            }
+
+
+            // 从payload获取userid
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+            // 返回 jsonwebton 
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                });
+
+        }
+        catch (err) {
+            console.log(err.message);
+            res.status(500).send('Server');
+        }
+
+
+    });
 module.exports = router;
 
